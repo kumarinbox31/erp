7bd96688cb7a5ceb9ab92e14be12c8b3cbecd736
kumarinbox31 <kumarinbox31@gmail.com>
2024-07-12 22:35:31 +0530

    env path declare

--- htdocs/web_api.php
+++ htdocs/web_api.php
@@ -18,7 +18,7 @@ function loadEnv($path)
 
 // Load .env file (assuming it's in the same directory as this script)
 $dotenvPath = dirname(__DIR__) . '/htdocs/.env';
-echo $dotenvPath;exit;
+// echo $dotenvPath;exit;
 try {
     loadEnv($dotenvPath);
 } catch (Exception $e) {
